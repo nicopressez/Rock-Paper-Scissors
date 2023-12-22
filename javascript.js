3962d0e22226e7b9103daa3128179908342d98a7
nicopressez <154272808+nicopressez@users.noreply.github.com>
2023-12-22 18:56:02 +0100

    Cleaned up code
    Game loop & round choice array added and code cleaned up.

--- javascript.js
+++ javascript.js
@@ -1,104 +1,76 @@
-// Initialize player and computer score
-
 let playerScore = 0;
 let computerScore = 0;
 
-// The computer randomly decides rock paper or scissors 
+// Random computer choice
 
 function getComputerChoice() {
-min = Math.ceil(0);
-max = Math.floor(2);
-randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-if (randomNumber == 0) 
-{
-  return "Rock";
-}
-else if (randomNumber == 1)
-{
-    return "Paper";
-}
-else if (randomNumber == 2)
-{
-    return "Scissors"
-}
+ min = Math.ceil(0);
+ max = Math.floor(2);
+ randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+ const roundChoice = ["Rock" , "Paper", "Scissors"];
+ return roundChoice[randomNumber];
 };
 
-// Get player choice based on prompt input
-// If player types something else, ask again
-
 function getPlayerChoice() {
-    playerInput = prompt("Rock Paper or Scissors?");
-    if (playerInput.toLowerCase() == "rock")
+ playerInput = prompt("Rock Paper or Scissors?");
+ if (playerInput.toLowerCase() == "rock")
     {
     return "Rock";
     }
-    else if (playerInput.toLowerCase() == "paper")
+ else if (playerInput.toLowerCase() == "paper")
     {
     return "Paper";
     }
-    else if (playerInput.toLowerCase() == "scissors")
+ else if (playerInput.toLowerCase() == "scissors")
     {
     return "Scissors";
     }
-    else 
+ else 
     {
     console.log("Choice incorrect, please choose between the 3 options")
     getPlayerChoice();
     }
 }
 
-// Plays a single round of Rock Paper Scissors
-// Determine the winner and update player scores
-// Display computer's choice + the result of the round
-
 function playRound () {
-let    playerSelection = getPlayerChoice();
-let    computerSelection = getComputerChoice();
+ let playerSelection = getPlayerChoice();
+ let computerSelection = getComputerChoice();
 
-if (playerSelection == "Rock" && computerSelection == "Scissors" || 
+ if (playerSelection == "Rock" && computerSelection == "Scissors" || 
     playerSelection == "Paper" && computerSelection == "Rock" ||
     playerSelection== "Scissors" && computerSelection == "Paper")
-{
-    console.log ("The computer chose:" + " " + computerSelection)
-    console.log("You win! " + playerSelection + " beats " + computerSelection);
+ {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
     return playerScore++;
-}
-else if (playerSelection == computerSelection)
-{
-    console.log ("The computer chose:" + " " + computerSelection)
+ }
+ else if (playerSelection == computerSelection)
+ {
     console.log ("It's a tie!");
-}
-else
-{
-    console.log ("The computer chose:" + " " + computerSelection)
-    console.log("Computer wins! " + computerSelection + " beats " + playerSelection );
+ }
+ else
+ {
+    console.log(`Computer wins! ${computerSelection} beats ${playerSelection}`);
     return computerScore++;
-}
-}
-
-// 5 rounds are played, the computer's and player scores show on console.
-// Todo: Make this part a loop when revamp
-
-function game() {
-    playRound();
-    console.log ("Your current score is: " + playerScore);
-    console.log ("The computer's score is: " + computerScore);
-    playRound();
-    console.log ("Your current score is: " + playerScore);
-    console.log ("The computer's score is: " + computerScore);
-    playRound();
-    console.log ("Your current score is: " + playerScore);
-    console.log ("The computer's score is: " + computerScore);
-    playRound();
-    console.log ("Your current score is: " + playerScore);
-    console.log ("The computer's score is: " + computerScore);
-    playRound();
-    console.log ("Your current score is: " + playerScore);
-    console.log ("The computer's score is: " + computerScore);
-    
+ }
 }
 
-game();
+// Rounds are played until one of the player reaches 5 points
 
+function startGameLoop(){
+   for (let index = 0; playerScore < 5 && computerScore < 5; index++) {
+      playRound();
+    console.log ("Your current score is: " + playerScore);
+    console.log ("The computer's score is: " + computerScore);;
+      
+   }
+   if (playerScore == 5)
+   {
+      console.log("You won the game");
+   }
+   else if (computerScore == 5)
+   {
+      console.log("You lost the game.")
+   }
+}
 
-// Todo: Game stops when a player reaches score "5" 
+startGameLoop();
